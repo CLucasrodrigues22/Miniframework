e193de53fa8888a0b46474df88377f1017325e41
CLucasrodrigues22 <c.lucasrodrigues22@gmail.com>
2020-07-29 22:33:21 -0300

    adicionando controllers

---
+++ App/Controllers/indexController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controllers;
+
+class IndexController {
+
+	public function index() {
+
+		echo 'Chegamos ao Index Controller e disparamos a action index';
+
+	}
+
+	public function sobreNos() {
+
+		echo 'chegamos ao index controller e disparamos a action sobreNos';
+
+	}
+}
